8194b2ac16317c584fa99fbaf3691551bf5e4424
xyzCh <hsn940814@live.com>
2018-12-09 15:37:32 +0800

    Add files via upload

--- Routing/Routing.cs
+++ Routing/Routing.cs
@@ -1,1 +1,23 @@
+﻿using System;
+using System.Web;
+using System.Reflection;
+using System.Collections.Generic;
 
+
+namespace RoutingList
+{
+    internal class Routing
+    {
+        public IHttpHandler handler { get; set; }
+        public Dictionary<string, RoutingAction> actions { get; set; }
+        public DateTime modifyDate { get; set; }
+    }
+
+    internal class RoutingAction
+    {
+        public MethodInfo action { get; set; }
+        public string actionName { get; set; }
+        public bool IsDefAction { get; set; }
+        public DateTime modifyDate { get; set; }
+    }
+}
---
+++ Routing/RoutingCache.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+
+namespace RoutingList
+{
+
+    internal class routerTable
+    {
+        private ReaderWriterLockSlim cacheLock = new ReaderWriterLockSlim();
+        private Dictionary<string, Routing> _cache = new Dictionary<string, Routing>();
+
+        public Routing ReadRouting(string url)
+        {
+            cacheLock.EnterReadLock();
+            try
+            {
+                Routing routing=null;
+                _cache.TryGetValue(url, out routing);
+                return routing;
+            }
+            finally
+            {
+                cacheLock.ExitReadLock();
+            }
+        }
+
+        public RoutingAction ReadAction(string url,string key) {
+            cacheLock.EnterReadLock();
+            try
+            {
+                RoutingAction action = null;
+                _cache[url].actions.TryGetValue(key,out action);
+                return action;
+            }
+            finally
+            {
+                cacheLock.ExitReadLock();
+            }
+        }
+
+        public void AddRouting(string url, Routing routing)
+        {
+            cacheLock.EnterWriteLock();
+            try
+            {
+                _cache[url] = routing;
+            }
+            finally
+            {
+                cacheLock.ExitWriteLock();
+            }
+        }
+
+        public void AddOrUpdateAction(string url,string key, RoutingAction action)
+        {
+            cacheLock.EnterUpgradeableReadLock();
+            try
+            {
+                RoutingAction result = null;
+                if (_cache[url].actions.TryGetValue(key,out result))
+                {
+                    {
+                        cacheLock.EnterWriteLock();
+                        try
+                        {
+                            result.action = action.action;
+                            result.actionName = action.actionName;
+                            result.IsDefAction = action.IsDefAction;
+                            result.modifyDate = action.modifyDate;
+                        }
+                        finally
+                        {
+                            cacheLock.ExitWriteLock();
+                        }
+                    }
+                }
+                else
+                {
+                    cacheLock.EnterWriteLock();
+                    try
+                    {
+                        _cache[url].actions.Add(key, action);
+                    }
+                    finally
+                    {
+                        cacheLock.ExitWriteLock();
+                    }
+                }
+            }
+            finally
+            {
+                cacheLock.ExitUpgradeableReadLock();
+            }
+        }
+
+        ~routerTable()
+        {
+            if (cacheLock != null) cacheLock.Dispose();
+        }
+    }
+}
